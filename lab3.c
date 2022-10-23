5ffaedd8eb0983910ba552b42615c67480d01c2c
DmitriMAI <106885046+DmitriMAI@users.noreply.github.com>
2022-10-23 13:40:50 +0300

    V 2.0
    Finally working multithreading

--- lab3.c
+++ lab3.c
@@ -1,81 +1,69 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/times.h>
 #include <time.h>
-#include <pthread.h>
 
-void *thread_body(void* args);
-
-	typedef struct {
-		int to_do;
-		int radius;
-	} arg_for_t;
-
-
-int count_mutex = 0;
-pthread_mutex_t mutex;
-
-int main(void) {
-
-    struct timespec start, end;
-    clock_gettime(CLOCK_REALTIME, &start);
-
-	srand(time(NULL));
-	
-	int num_thread;
-	scanf("%d", &num_thread);
-    pthread_t threads[num_thread];
-    arg_for_t t_arg[num_thread];
-	int c;	
-	scanf ("%d", &c);
-	pthread_mutex_init(&mutex, NULL);
-    for (int i = 0; i < num_thread; ++i) {
-		t_arg[i].radius = c;
-		t_arg[i].to_do = 4000000 /num_thread;
-		if ( i == 0) {
-			t_arg[i].to_do += 4000000 % num_thread;
-		}
-		printf ("%d \n", t_arg[i].to_do);
-        if (pthread_create(&(threads[i]), NULL, thread_body , &(t_arg[i]))) {
-            perror("Create thread error");
-            return -1;
+long incircle = 0;
+long radius = 0;
+long ppt; /* points per thread*/
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+void *runner() {
+    long incircle_thread = 0;
+    
+    unsigned int rand_state = rand();
+    long i;
+    for (i = 0; i < ppt; i++) {
+        double x = rand_r(&rand_state) / ((double)RAND_MAX + 1) * 2* radius - (radius);
+        double y = rand_r(&rand_state) / ((double)RAND_MAX + 1) * 2*radius - (radius);
+	//printf ("%lf %lf\n\n", x, y);
+        if (x * x + y * y <= (radius* radius)) {
+            incircle_thread++;
         }
     }
-	
-    for (int i = 1; i <= num_thread; ++i) {
-        if (pthread_join(threads[i - 1], NULL))
-        {
-            fprintf(stderr, "Error joining thread\n");
-            return 2;
-        }
-    }
-
-	double coef = (count_mutex/4000000.0);
-
-	printf("res = %lf\n", coef*2*c*2*c );
-
-    clock_gettime(CLOCK_REALTIME, &end);
-    double time_spent = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
-    printf("The elapsed time is %f seconds\n", time_spent);
-
-	pthread_mutex_destroy(&mutex);
+    pthread_mutex_lock(&mutex);
+    incircle += incircle_thread;
+    pthread_mutex_unlock(&mutex);
+	return NULL;
 }
 
-void *thread_body(void* args) {
-    arg_for_t *nw = args;
-	double x, y;
-	int local_sum_count = 0;
-	int c = nw->radius;
-	for (int i = 0; i < nw->to_do ; i++) {
-		x = (rand()%(c*100000))/(double)100000;
-		y = (rand()%(c*100000))/(double)100000;
-		if ((x*x + y*y) <= (c*c)){
-			(local_sum_count) ++;	
-		}
-	}
+int main(int argc, const char *argv[])
+{
+    struct timespec start_t, end;
+    clock_gettime(CLOCK_REALTIME, &start_t);
 
-	pthread_mutex_lock(&mutex);
-	count_mutex += local_sum_count;
-	pthread_mutex_unlock(&mutex);
+    if (argc != 4) {
+        fprintf(stderr, "usage: ./pi <total points> <threads>\n");
+        exit(1);
+    }
+    long totalpoints = atol(argv[1]);
+    int thread_count = atoi(argv[2]);
+	long nradius = atoi(argv[3]);
+	radius = nradius;
+    ppt = totalpoints / thread_count;
+
+    time_t start = time(NULL);
+    srand((unsigned)time(NULL));
+    pthread_t *threads = malloc(thread_count * sizeof(pthread_t));
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    int i;
+    for (i = 0; i < thread_count; i++) {
+        pthread_create(&threads[i], &attr, runner, (void *) NULL);
+    }
+    for (i = 0; i < thread_count; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    pthread_mutex_destroy(&mutex);
+    free(threads);
+
+	clock_gettime(CLOCK_REALTIME, &end);
+    double time_spent = (end.tv_sec - start_t.tv_sec) + (end.tv_nsec - start_t.tv_nsec) / 1000000000.0;
+    printf("\n\nThe elapsed time is %f seconds\n", time_spent);
+
+	double coef = (double)incircle / ((double)ppt * thread_count);
+	
+	printf ("%ld %ld %d\n", incircle, ppt,thread_count);
+    printf("Plosh: %lf \n", coef*2*radius*2*radius);
+    printf("Time: %d sec\n", (unsigned int)(time(NULL) - start));
     return 0;
 }
